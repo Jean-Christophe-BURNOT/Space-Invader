bc49fb5df3edab19b0d427f3210c48c0a1dcc79b
Jean-Christophe <jean-christophe.burnot@cpe.fr>
2021-12-14 12:10:39 +0100

    Creation class en

---
+++ classEnnemis.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Jean-Christophe BURNOT
+12/12/2021
+classe principale pour les ennemis
+"""
+class ennemis:
+    def __ini__(self,niveau):
+        self.__niveau = niveau
+        
+
+
+
+
